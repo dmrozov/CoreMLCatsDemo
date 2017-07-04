edd8b8b935d6b8d7033885e1e9e65ee7b3f50765
DmitriyLis <expertgames@yandex.ru>
2017-07-04 21:55:27 +0300

    Initial Commit

---
+++ CoreMLCatsDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreMLCatsDemo
+//
+//  Created by Dmitriy Lis on 04/07/2017.
+//  Copyright © 2017 Dmitriy Rozov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
